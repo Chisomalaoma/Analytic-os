636bc599f5f46884356d4ec1fce3f468efd62fa5
farazahmed7530 <f2020105101@umt.edu.pk>
2026-02-06 00:48:31 +0500

    Remove focus ring from toggle to fix white screen issue

--- src/common/ToggleSwitch.tsx
+++ src/common/ToggleSwitch.tsx
@@ -13,7 +13,7 @@ const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ checked, onChange }) => (
             onChange={(e) => onChange(e.target.checked)}
             className="sr-only peer"
         />
-        <div className="relative w-11 h-6 bg-gray-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-[#4459FF]/20 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#4459FF]"></div>
+        <div className="relative w-11 h-6 bg-gray-600 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#4459FF]"></div>
     </label>
 );
 
